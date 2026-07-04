497cdd6cd2b36c8868e311ca267bb8284842ea12
amir abaris <thisisamirabaris@gmail.com>
2026-07-04 18:16:51 +0330

    Implement Sum function to calculate the total of all numbers in the slice

--- kata/sum_numbers/solution.go
+++ kata/sum_numbers/solution.go
@@ -3,6 +3,11 @@ package sum_numbers
 // Sum returns the total of all numbers in nums.
 // Empty slice returns 0.
 func Sum(nums []int) int {
-	// TODO: implement
-	return 0
+	var total int 
+
+	for _, num := range nums {
+		total += num
+	}
+
+	return total
 }
